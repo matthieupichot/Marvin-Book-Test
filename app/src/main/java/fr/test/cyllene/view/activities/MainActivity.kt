cd71f34ed69411253dfcbc3c85e4ff5874c5a443
matthieu <pichotmatthieu@gmail.com>
2020-08-02 01:55:13 +0200

    travail sur la navigation entre l'écran Home et l'écran des favoris

--- app/src/main/java/fr/test/cyllene/view/activities/MainActivity.kt
+++ app/src/main/java/fr/test/cyllene/view/activities/MainActivity.kt
@@ -1,13 +1,52 @@
 package fr.test.cyllene.view.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.test.cyllene.R
+import fr.test.cyllene.view.fragments.FavoriteFragment
+import fr.test.cyllene.view.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState == null) {
+            val fragment = HomeFragment()
+            loadFragment(fragment)
+        }
+
+        bottom_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
+
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_book -> {
+                val fragment = HomeFragment()
+                loadFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_favorites -> {
+                val fragment = FavoriteFragment()
+                loadFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
 }

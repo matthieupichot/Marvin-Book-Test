1f825e63a69a6b5fb21d0f417582506ea6a49aa6
matthieu <pichotmatthieu@gmail.com>
2020-08-02 00:58:57 +0200

    création de la bottom navigation bar (écran Home)

--- app/src/main/java/fr/test/cyllene/view/activities/LoginActivity.kt
+++ app/src/main/java/fr/test/cyllene/view/activities/LoginActivity.kt
@@ -1,13 +1,20 @@
 package fr.test.cyllene.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import fr.test.cyllene.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_sign_in.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            finish()
+        }
 
     }
 }
---
+++ app/src/main/java/fr/test/cyllene/view/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package fr.test.cyllene.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.test.cyllene.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
